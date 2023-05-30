98e566cdbb2bb8ade620c7536593cbdf02e7aa8e
Tomas Votroubek <tomas.votroubek@outlook.com>
2023-05-30 03:22:25 +0200

    undo debug mac prefix

--- scripts/query_poseidons.sh
+++ scripts/query_poseidons.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#ip neigh | grep -i '00:0a:59:0.:..:..' | cut -f1,6 -d' '
-
-ip neigh | grep -i '..:..:..:..:..:..' | cut -f1,6 -d' '
+ip neigh | grep -i '00:0a:59:0.:..:..' | cut -f1,6 -d' '
